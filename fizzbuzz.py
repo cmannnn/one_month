ca8b2719c6ab20339a3920248c6ff1f6f690ec30
cmannnn <cmurphy1465@gmail.com>
2022-01-03 13:10:04 -0500

    fizzbuzz modification

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,10 @@
 # fizzbuzz challenge
 for i in range(1, 101):
-	if i % 3 == 0:
+	if i % 3 == 0 & i % 5 == 0:
+		print('FizzBuzz')
+	elif i % 3 == 0:
 		print('Fizz')
 	elif i % 5 == 0:
 		print("Buzz")
-	elif i % 3 == 0 & i % 5 == 0:
-		print('FizzBuzz')
 	else:
 		print(i)
